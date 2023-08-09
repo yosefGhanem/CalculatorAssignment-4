4353114e556b8c3fa2f1d7e54f6d4c7e57568eae
yosefGhanem <yousefghanem.linkedin@gmail.com>
2023-08-10 00:43:24 +0300

    commit

--- scripts.js
+++ scripts.js
@@ -1,23 +1,68 @@
+class Calculator {
+    constructor(displayElementId) {
+        this.display = document.getElementById(displayElementId);
+        this.operationsButtons = document.querySelectorAll('.operator, .digit, .equal, .clear, .backspace, .percent, .digitzero, .sign');
+        this.currentInput = '';
+        this.attachButtonListeners();
+    }
 
-const display = document.getElementById('display');
-const buttons = document.querySelectorAll('button');
+    attachButtonListeners() {
+        this.operationsButtons.forEach(button => {
+            button.addEventListener('click', () => this.handleButtonClick(button.textContent));
+        });
+    }
 
-buttons.forEach(button =>{
-    button.addEventListener('click', () => {
-        
-        if(button.textContent === '='){
+    handleButtonClick(buttonText) {
+        if (buttonText === '=') {
             try {
-                //display.value
+                this.currentInput = this.currentInput.replace(/X/g, '*');
+                const result = eval(this.currentInput);
+                this.display.innerText = result;
+                this.currentInput = result.toString();
             } catch (error) {
-                display.innerText = 'Error';
+                this.display.innerText = 'Error';
             }
-        }else if(button.textContent === 'AC'){
-            display.innerText = null;
-        }else if(button.textContent === 'BACK'){
-            display.innerText = display.innerText.slice(0, -1); //To remove when pressing BACK
-        }else{
-            display.innerText += button.textContent;
+            if(this.currentInput === '=' && this.currentInput != "") {
+                setTimeout(() => {
+                    this.display.innerText = '';
+                    this.currentInput = '';
+                }, 2000);
+            }
+            
+        } else if (buttonText === 'AC') {
+            this.display.innerText = '';
+            this.currentInput = '';
+        } else if (buttonText === 'BACK') {
+            this.currentInput = this.currentInput.slice(0, -1);
+            this.display.innerText = this.currentInput;
+        } else if (buttonText === '+/-') {
+            if (this.currentInput !== '' && this.currentInput !== '=') {
+                const num = parseFloat(this.currentInput);
+                this.currentInput = (-num).toString();
+                this.display.innerText = this.currentInput;
+            }
+        } else {
+            if (this.currentInput === '=') {
+                this.currentInput = '';
+                this.display.innerText = '';
+            } else if (this.currentInput.includes("=")) {
+                this.currentInput = buttonText;
+                this.display.innerText = buttonText;
+            } else {
+                if (buttonText === 'X') {
+                    this.currentInput += '*';
+                } else if (buttonText === '/') {
+                    this.currentInput += '/';
+                } else if (buttonText === '%') {
+                    this.currentInput += '%';
+                } else {
+                    this.currentInput += buttonText;
+                }
+                this.display.innerText = this.currentInput;
+            }
         }
+    }
+}
 
-    });
-});
+const basicCalc = new Calculator('display');
+// Works perfectly
